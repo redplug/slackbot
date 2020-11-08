42fc10f2bc9a7dd8e84b8e9e0b992e0d184590b4
redplug <redplug@gmail.com>
2020-11-08 09:46:48 +0900

    db

--- slackchatbot.py
+++ slackchatbot.py
@@ -8,13 +8,9 @@ from slacker import Slacker
 
 from flask import Flask, request, make_response
 
-import random
-
 import requests
 
 from bs4 import BeautifulSoup
-
-import atexit
 
 import string
 
@@ -25,12 +21,6 @@ from selenium import webdriver
 from urlshort import url_short
 
 from bob_recommend import food_answer
-
-from selenium.webdriver.common.keys import Keys
-
-from selenium.webdriver.chrome.options import Options
-
-import datetime
 
 bob = "밥"
 
@@ -48,41 +38,6 @@ slack = Slacker(os.environ.get('SLACK_BOT_TOKEN'))
 botid = os.environ.get('SLACK_BOT_ID')
 
 app = Flask(__name__)
-
-
-def get_answer():
-    food = ['<https://map.naver.com/v5/search/%ED%9D%AC%EB%9E%98%EB%93%B1/place/31398765?c=14141304.0776130,4508977.3027141,13,0,0,0,dh|*희래등*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EC%96%8C%EC%83%98%EA%B9%80%EB%B0%A5/place/1411505334?placeSearchOption=fromNxList=true%26noredirect=1%26entry=pll&c=14141012.6614427,4510515.5979084,13,0,0,0,dh&placePath=%2Fhome%3Fentry=pll|*얌샘*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EB%B6%80%EC%82%B0%EC%95%84%EC%A7%80%EB%A7%A4/place/1161628944?c=14143649.7389185,4509919.6279535,15,0,0,0,dh|*부산아지매*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EB%82%A8%EC%82%B0%EC%98%A5/place/20259836?c=14143649.7389185,4509919.6279535,15,0,0,0,dh|*남산옥*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EB%9D%BD%EC%8A%A4%ED%94%8C%EB%A0%88%EC%9D%B4%ED%8A%B8/place/19536440?c=14143649.7389185,4509919.6279535,15,0,0,0,dh|*락스플레이트*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EA%B5%90%EB%8F%99%EC%A0%84%EC%84%A0%EC%83%9D/place/36160929?c=14143936.3777746,4508912.8089715,14,0,0,0,dh|*교동전선생*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EA%B8%B8%EB%86%8D%EC%9B%90/place/34520779?c=14143936.3777746,4508912.8089715,14,0,0,0,dh|*길농원*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EB%AA%85%EB%8F%99%EC%B9%BC%EA%B5%AD%EC%88%98/place/1551854356?c=14143926.8231461,4510237.3193523,15,0,0,0,dh|*명동칼국수*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EB%8F%99%ED%95%B4%ED%9A%9F%EC%A7%91?c=14144464.2710012,4509907.6846678,15,0,0,0,dh|*동해횟집*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EB%82%A8%EC%9B%90%EC%B6%94%EC%96%B4%ED%83%95/place/20624013?c=14144486.9632440,4510261.8030879,16,0,0,0,dh|*남원추어탕*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EB%B0%B1%EA%B5%AC%EC%8B%9D%EB%8B%B9/place/12884436?c=14144563.4002722,4510162.6738168,16,0,0,0,dh|*백구식당*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EC%82%BC%EA%B5%B0%EA%B9%80%EC%B9%98%EC%B0%8C%EA%B0%9C/place/757870494?c=14144024.7580885,4510270.7605521,15,0,0,0,dh&placePath=%3F%2526|*삼군김치찌개*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EC%82%BC%EC%84%B1%EA%B3%A8/place/495083414?c=14143871.8840320,4510215.8214381,15,0,0,0,dh&placePath=%3F%2526|*삼성골*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EC%86%A1%EB%8B%B4%EC%B6%94%EC%96%B4%ED%83%95/place/1951213845?c=14141031.7706998,4510563.3710510,13,0,0,0,dh&placePath=%3F%2526|*송담추어탕*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EC%98%88%EC%9E%90%EB%84%A4%EB%B0%A5%EC%83%81/place/37997070?c=14141241.9725275,4510458.2701372,13,0,0,0,dh|*예자네밥상*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EC%9A%B0%EB%A6%AC%EC%A7%91/place/18824809?c=14143874.2726891,4510256.4286093,15,0,0,0,dh|*우리집*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EC%9D%B4%ED%83%9C%EB%A6%AC%EB%B6%80%EB%8C%80%EC%B0%8C%EA%B0%9C/place/1763681004?c=14143076.4612064,4510350.7805661,14,0,0,0,dh|*이태리부대찌개*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EC%9E%84%EA%B3%A0%EC%A7%91%ED%95%9C%EC%9A%B0/place/13192719?c=14143076.4612064,4510350.7805661,14,0,0,0,dh|*임고집한우*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EC%A0%84%EC%A3%BC%EC%BD%A9%EB%82%98%EB%AC%BC%EA%B5%AD%EB%B0%A5/place/32602528?c=14143100.3477777,4510374.6671375,14,0,0,0,dh|*전주콩나물국밥*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EC%A1%B1%EB%B0%9C%EB%B3%B4%EC%8C%88%EB%A7%88%EC%9D%84/place/32594023?c=14143100.3477777,4510374.6671375,14,0,0,0,dh|*족발보쌈마을*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EC%B9%B4%EB%A0%88%EB%A7%88%EC%B9%98/place/37130941?c=14143100.3477777,4510374.6671375,14,0,0,0,dh|*카레마치*>', \
-        '<http://freshville.co.kr/|*후레쉬빌*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%EC%82%AC%EC%A1%B0%EC%B0%B8%EC%B9%98/place/13175130?c=14144574.1492293,4510556.8022439,16,0,0,0,dh|*사조참치*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%ED%8F%AC%ED%8F%AC%EB%B9%88/place/1215767360?c=14144574.1492293,4510556.8022439,16,0,0,0,dh|*포포빈*>', \
-        '<https://map.naver.com/v5/search/%EB%8C%80%EC%B9%98%EB%8F%99%20%ED%81%AC%EB%9D%BC%EC%9D%B4%EB%B2%84%EA%B1%B0/place/587929920?c=14144574.1492293,4510556.8022439,16,0,0,0,dh|*크라이*>', \
-        '<|*배달*>', \
-        '<|*여울이도 밥주세오*>', \
-        '<https://map.naver.com/v5/entry/place/13172001?c=14143887.9251101,4509407.0234595,15,0,0,0,dh&placePath=%3Fentry=plt|*희래등*>']
-
-    random.shuffle(food)
-    return food
-
 
 def get_weather():
     location = "대치동"
