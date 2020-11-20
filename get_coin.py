5a90b3bcec16923ef4d6ea461280cc6f51ceb781
redplug <redplug@gmail.com>
2020-11-21 07:50:28 +0900

    coin update

--- get_coin.py
+++ get_coin.py
@@ -11,6 +11,8 @@ def get_coin(coin):
         ticker = coin.replace("코인","")
         price = format(pyupbit.get_current_price(ticker), ',')
         df = pyupbit.get_ohlcv(ticker, count=1, interval="day")
-        del df['volume','low','high']
+        del df['volume']
+        del df['low']
+        del df['high']
         df['Date'] = df.index
         return df, price
